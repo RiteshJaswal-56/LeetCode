ac03120f3da7eb552a494555c4f2294f9db6f16c
Ritesh Jaswal <riteshjaswal56@gmail.com>
2025-12-13 15:18:58 +0000

    Sync LeetCode submission - Reverse Linked List (java)

--- past-submissions/problems/reverse_linked_list/solution.java
+++ past-submissions/problems/reverse_linked_list/solution.java
@@ -11,8 +11,8 @@
 class Solution {
     public ListNode reverseList(ListNode head) {
         ListNode prev = null;
-        ListNode current = head ;
-        while (current != null){
+        ListNode current = head;
+        while(current!=null){
             ListNode nextnode = current.next;
             current.next = prev;
             prev = current;
