a24ac6df2950e803c0c208df7f899da275b99446
Ritesh Jaswal <riteshjaswal56@gmail.com>
2026-02-13 12:33:41 +0000

    Sync LeetCode submission - Permutations (java)

---
+++ past-submissions/problems/permutations/solution.java
@@ -0,0 +1,19 @@
+class Solution {
+    public List<List<Integer>> permute(int[] nums) {
+        List<List<Integer>> result = new ArrayList<>();
+        backtrack(nums, result, new ArrayList<>());
+        return result;
+    }
+    private void backtrack(int[] nums, List<List<Integer>> result, List<Integer> temp) {
+        if (temp.size() == nums.length) {
+            result.add(new ArrayList<>(temp));
+            return;
+        }
+        for (int i = 0; i < nums.length; i++) {
+            if (temp.contains(nums[i])) continue;
+            temp.add(nums[i]);
+            backtrack(nums, result, temp);
+            temp.remove(temp.size() - 1);
+        }
+    }
+}
