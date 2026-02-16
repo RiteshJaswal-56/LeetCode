293d54f2bed56d2213c0b0e7c78966d9093fee6c
Ritesh Jaswal <riteshjaswal56@gmail.com>
2026-02-16 05:11:05 +0000

    Sync LeetCode submission - Combination Sum (java)

---
+++ past-submissions/problems/combination_sum/solution.java
@@ -0,0 +1,19 @@
+class Solution {
+    public List<List<Integer>> combinationSum(int[] candidates, int target) {
+        List<List<Integer>> result = new ArrayList<>();
+        backtrack(candidates , target , 0 ,result , new ArrayList<>());
+        return result;
+    }
+    private void backtrack( int []candidates ,int target , int start ,List<List<Integer>> result, List<Integer> temp){
+        if(target==0){
+            result.add(new ArrayList<>(temp));
+            return;
+        }
+        for(int i=start;i<candidates.length;i++){
+            if(target<0) return;
+            temp.add(candidates[i]);
+            backtrack(candidates,target-candidates[i],i,result,temp);
+            temp.remove(temp.size()-1);
+        }
+    }
+}
