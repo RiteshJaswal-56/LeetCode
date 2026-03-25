5bff7f59ad8d9faf5823e05909829df3d619de17
Ritesh Jaswal <riteshjaswal56@gmail.com>
2026-03-25 12:50:55 +0000

    Sync LeetCode submission - Combination Sum III (java)

--- past-submissions/problems/combination_sum_iii/solution.java
+++ past-submissions/problems/combination_sum_iii/solution.java
@@ -4,14 +4,14 @@ class Solution {
         backtrack(k,n,1,result,new ArrayList<>());
         return result;
     }
-    private void backtrack(int k,int n,int start,List<List<Integer>> result,List<Integer> temp){
+    private void backtrack(int k, int n, int index, List<List<Integer>> result, List<Integer> temp){
         if(temp.size() == k){
-            if(n == 0){
+            if(n==0){
                 result.add(new ArrayList<>(temp));
                 return;
             }
         }
-        for(int i=start;i<=9;i++){                                               
+        for(int i=index;i<=9;i++){
             temp.add(i);
             backtrack(k,n-i,i+1,result,temp);
             temp.remove(temp.size()-1);
