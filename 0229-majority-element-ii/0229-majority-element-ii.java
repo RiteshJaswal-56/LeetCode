b0aa44b886497f7bbc75def128224af0e60bee94
Ritesh Jaswal <riteshjaswal56@gmail.com>
2026-07-23 11:40:02 +0530

    Time: 691 ms (5.74%), Space: 52.5 MB (66.07%) - LeetHub

--- 0229-majority-element-ii/0229-majority-element-ii.java
+++ 0229-majority-element-ii/0229-majority-element-ii.java
@@ -4,16 +4,17 @@ class Solution {
         List<Integer> result = new ArrayList<>();
 
         for(int i=0;i<n;i++){
-
             if(result.contains(nums[i])){
                 continue;
             }
+            
             int count = 0;
             for(int j=i;j<n;j++){
                 if(nums[i] == nums[j]){
                     count++;
                 }
             }
+
             if(count > n/3){
                 result.add(nums[i]);
             }
