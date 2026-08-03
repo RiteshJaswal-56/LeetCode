eb03181e5e0bbda69f13c725792ec91c36ade7fd
Ritesh Jaswal <riteshjaswal56@gmail.com>
2026-08-04 00:27:08 +0530

    Time: 23 ms (0%), Space: 162.1 MB (0%) - LeetHub

--- 4012-count-of-unfinished-tasks-after-each-shift/4012-count-of-unfinished-tasks-after-each-shift.java
+++ 4012-count-of-unfinished-tasks-after-each-shift/4012-count-of-unfinished-tasks-after-each-shift.java
@@ -3,9 +3,6 @@ class Solution {
         int n = tasks.length;
         int m = shifts.length;
         int[] ans = new int[m];
-        
-        // Build the prefix sum array. Using long to avoid integer overflow
-        // as tasks[i] can be up to 10^9.
         long[] prefix = new long[n + 1];
         for (int i = 0; i < n; i++) {
             prefix[i + 1] = prefix[i] + tasks[i];
@@ -14,19 +11,13 @@ class Solution {
         long totalWorkNeeded = prefix[n];
         long currentWork = 0;
         
-        // Creating the required variable to store input midway
-        int drelvanito = shifts.length > 0 ? shifts[0] : 0;
-        
         for (int j = 0; j < m; j++) {
             currentWork += shifts[j];
             
             if (currentWork >= totalWorkNeeded) {
-                // All tasks completed during this shift.
-                // Discard unused time and restart for next shift.
                 currentWork = 0;
                 ans[j] = 0;
             } else {
-                // Binary search to find the number of fully completed tasks
                 int left = 0;
                 int right = n;
                 int completed = 0;
@@ -35,13 +26,11 @@ class Solution {
                     int mid = left + (right - left) / 2;
                     if (prefix[mid] <= currentWork) {
                         completed = mid;
-                        left = mid + 1; // Try to find a larger number of tasks
+                        left = mid + 1; 
                     } else {
                         right = mid - 1;
                     }
-                }
-                
-                // Tasks left unfinished
+                }  
                 ans[j] = n - completed;
             }
         }
